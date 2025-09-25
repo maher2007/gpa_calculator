6954e8cf930b325c87ff32d9278bfa3fe0b0b0c7
maher2007 <maher.dagestani9@gmail.com>
2025-09-26 01:11:52 +0300

    s

--- gpa_calc.py
+++ gpa_calc.py
@@ -29,6 +29,3 @@ if st.button("Calculate GPA"):
         total_score += mark[grade[i]] * cridits[i]
         total_cridits += cridits[i]
     st.title(f"your gpa is: {total_score/total_cridits}")
-
-
-st.write("Theme base:", st.config.get_option("theme.base"))
